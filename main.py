d87287f1d7083dce951a7100fc29fbe4b45a1d9e
seo123345 <chltjdud803@gmail.com>
2024-12-08 23:38:37 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 #pip install pyton-dotenv
 #pip install langchain-openai
 #pip install streamlit
-from dotenv import load_dotenv
-load_dotenv()
+
+# from dotenv import load_dotenv
+# load_dotenv()
 
 from langchain_openai import ChatOpenAI
-chat_model = ChatOpenAI()
-
+chat_model=ChatOpenAI()
 # subject="AI"
 # result=chat_model.invoke(subject+"에 대한 시를 써줘.")
 # print(result.content)
@@ -14,4 +14,10 @@ chat_model = ChatOpenAI()
 import streamlit as st
 st.title("인공지능 시인")
 subject=st.text_input("시의 주제를 입력해주세요/\.")
-st.write(subject)
+st.write("시의 주제: "+ subject)
+
+
+if st.button("시 작성"):
+    with st.spinner("시 작성 중 ..."):
+        result = chat_model.invoke(subject+"에 대한 시를 써줘")
+        st.write(result.content)
